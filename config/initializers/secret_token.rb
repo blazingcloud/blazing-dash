71ebac4cbcb1839d4bddb819faa928f2e2fda74a
Blazing Pair <blazingpair@blazingcloud.net>
2011-08-26 10:46:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlazingDash::Application.config.secret_token = '7bbd4bed80575adeb869fb76ad8175c97a6b7e1bc3eeb69f4b900883609345c0440b581608e6b38f523e0885ee1b61d632fc11851c892bb923af1c5182104f2d'
